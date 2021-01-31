78712a665ff7fa0a47d1ce0cd8c22b56af1ca1e3
ArielMejiaDev <arielmejiadev@gmail.com>
2021-01-30 19:56:23 -0600

    Hotfix on html template

--- App.vue
+++ App.vue
@@ -1,31 +1,25 @@
 <template>
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi sitio personal</title>
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body class="bg-gray-100 h-100">
-    
-    <div class="container mx-auto p-4">
+    <div class="bg-gray-100 h-100">
 
-        <contact-card/>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
-        <div class="mt-10" v-if="showSkills">
+        <div class="container mx-auto p-4">
 
-            <h2 class="m-2 font-semibold text-xl text-gray-800">Skills</h2>
+            <contact-card/>
 
-            <badge :title="skill.title" :link="skill.link" v-for="(skill, index) in skills" :key="index"/>
+            <div class="mt-10" v-if="showSkills">
+
+                <h2 class="m-2 font-semibold text-xl text-gray-800">Skills</h2>
+
+                <badge :title="skill.title" :link="skill.link" v-for="(skill, index) in skills" :key="index"/>
+
+            </div>
+
 
         </div>
 
-
     </div>
 
-</body>
-</html>
 </template>
 
 <script>
