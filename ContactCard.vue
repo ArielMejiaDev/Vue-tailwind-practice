d152afcd7463aa499b7bab7239bc5ba5e024dc88
ArielMejiaDev <arielmejiadev@gmail.com>
2021-01-30 18:40:02 -0600

    Initial codebase

---
+++ ContactCard.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <div class="bg-white p-6 shadow rounded w-full lg:w-3/6">
+        
+        <div class="flex flex-col lg:flex-row">
+            
+            <div>
+                <div class="title">Address</div>
+                <div class="mt-2 text-gray-500 font-light">
+                    Central America, Guatemala, Guatemala City.
+                </div>
+            </div>
+
+            <div class="mt-10 lg:mt-0 lg:ml-10">
+                <div>
+                    <div class="title">Email</div>
+                    <a href="#" target="_blank" class="bg-indigo-500 text-indigo-100 text-sm xl:text-base p-2 xl:py-3 xl:px-8 rounded flex text-center mt-2 hover:bg-indigo-600">Send me an email</a>
+                </div>
+
+                <div class="mt-6">
+                    <div class="title">Github</div>
+                    <a href="#" target="_blank" class="text-sm text-indigo-500 hover:underline">Github account</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+
+}
+</script>
+
+<style>
+    @apply {
+        .title {
+            uppercase text-sm tracking-wider font-medium
+        }
+    }
+</style>
