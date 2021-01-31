98ef388808d3c53681915ceed9616119446fa127
ArielMejiaDev <arielmejiadev@gmail.com>
2021-01-30 21:49:56 -0600

    Hotfix on styles and adding subtitle component

--- App.vue
+++ App.vue
@@ -1,18 +1,24 @@
 <template>
-    <div class="bg-gray-100 h-100">
+    <div class="bg-gray-100 h-screen">
 
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
         <div class="container mx-auto p-4">
 
-            <contact-card/>
+            <h1 class="text-gray-700 font-bold text-2xl underline my-10">Personal Site</h1>
 
-            <div class="mt-10" v-if="showSkills">
+                <div class="mt-10" v-if="showSkills">
 
-                <h2 class="m-2 font-semibold text-xl text-gray-800">Skills</h2>
+                <sub-title text="Skills" />
 
                 <badge :title="skill.title" :link="skill.link" v-for="(skill, index) in skills" :key="index"/>
 
+            </div>
+
+            <div class="my-10">
+                <sub-title text="Contact" />
+
+                <contact-card/>
             </div>
 
 
@@ -23,11 +29,13 @@
 </template>
 
 <script>
+import SubTitle from './components/SubTitle.vue';
 import ContactCard from './components/ContactCard.vue';
 import Badge from './components/Badge.vue';
 
 export default {
     components: {
+        SubTitle,
         ContactCard,
         Badge,
     },
---
+++ components/SubTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 class="m-2 font-semibold text-xl text-gray-800">{{ text }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+      text: String,
+  }
+}
+</script>
+
+<style>
+
+</style>
